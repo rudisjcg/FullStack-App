541f33395a572d6678754ed2ab71708b280a34a7
krakatua <rudisjcg@gmail.com>
2023-12-04 18:26:54 -0400

    Testing Google/Apple Pay

--- testing-nest-front/src/App.tsx
+++ testing-nest-front/src/App.tsx
@@ -1,60 +1,22 @@
-import { useState } from 'react'
 import './App.css'
+import MainPage from './components/MainPage';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+const stripePromise = loadStripe("pk_test_51MpY8VGTCtdGURLxuY8eEWuDW20TTYW62DGlT8JGuxcibBSCJDEle6UBxqJE1CJwXeSZ9EdbgM1lMthoxDqoPALP00Wdd4bSMc");
+
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [text, setText] = useState("");
-
-  const updatingUser = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const response = await fetch("http://localhost:3000/users", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, email, password })
-    })
-    const data = await response.json();
-    console.log(data);
-  }
-
-
-  const webHook = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const response = await fetch("http://localhost:3000/webhook", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ text })
-    })
-
-    console.log(response)
-  }
+  const options: any = {
+    mode: 'payment',
+    amount: 1099,
+    currency: 'usd',
+    appearance: {/*...*/},
+  };
 
   return (
-    <>
-      <form className='form_Wrapper' onSubmit={updatingUser}>
-      <label htmlFor="name">User Form</label>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" onChange={e => setName(e.target.value)} />
-        <label htmlFor="name">Email</label>
-        <input type="email" id="email" name="email" onChange={e => setEmail(e.target.value)}/>
-        <br />
-        <label htmlFor="email">Password</label>
-        <input type="password" id="email" name="password"  onChange={e => setPassword(e.target.value)}/>
-        <button type="submit">Submit</button>
-
-      </form>
-      
-      <form onSubmit={webHook}>
-        <label htmlFor="name">Webhook</label>
-        <label htmlFor="name">Text</label>
-        <input type="text" id="text" name="text" onChange={e => setText(e.target.value)} />
-        <button >Submit</button>
-
-      </form>
-
-    </>
+    <Elements stripe={stripePromise} options={options}>
+      <MainPage/>
+    </Elements>
   )
 }
 
---
+++ testing-nest-front/src/components/MainPage.tsx
@@ -0,0 +1,225 @@
+import React, { useState, useEffect } from 'react'
+import { ExpressCheckoutElement } from '@stripe/react-stripe-js';
+import { useStripe } from '@stripe/react-stripe-js';
+import { useElements } from '@stripe/react-stripe-js';
+import { StripeExpressCheckoutElementConfirmEvent } from '@stripe/stripe-js';
+import axios from 'axios';
+
+
+
+
+
+export default function MainPage() {
+
+  
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [text, setText] = useState("");
+    const [id, setId] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const stripe = useStripe();
+    const elements = useElements();
+    const [paymentRequest, setPaymentRequest] = useState({} || null);
+    const [messages, addMessage] = useState("");
+
+  useEffect(() => {
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const pr = stripe.paymentRequest({
+      country: 'US',
+      currency: 'usd',
+      total: {
+        label: 'Demo total',
+        amount: 1999,
+      },
+      requestPayerName: true,
+      requestPayerEmail: true,
+    });
+
+    console.log(pr)
+
+    // Check the availability of the Payment Request API.
+    pr.canMakePayment().then(result => {
+      console.log(result)
+      if (result) {
+        setPaymentRequest(pr);
+      }
+    });
+    console.log(paymentRequest)
+    pr.on('paymentmethod', async (e) => {
+      const response = await fetch('http://localhost:3500/create-intent', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          paymentMethodType: 'card',
+          currency: 'usd',
+        })
+      });
+
+      console.log(await response.json());
+
+      const { error: backendError, clientSecret } = await response.json();
+
+      if (backendError) {
+        addMessage(backendError.message);
+        return;
+      }
+
+      addMessage('Client secret returned');
+
+      const {
+        error: stripeError,
+        paymentIntent,
+      } = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: e.paymentMethod.id,
+      }, { handleActions: false });
+
+      if (stripeError) {
+        // Show error to your customer (e.g., insufficient funds)
+        addMessage(stripeError.message || "");
+        return;
+      }
+
+      // Show a success message to your customer
+      // There's a risk of the customer closing the window before callback
+      // execution. Set up a webhook or plugin to listen for the
+      // payment_intent.succeeded event that handles any business critical
+      // post-payment actions.
+      addMessage(`Payment ${paymentIntent.status}: ${paymentIntent.id}`);
+    });
+
+  }, [stripe, elements, addMessage]);
+  console.log(messages)
+
+    const updatingUser = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+  
+      
+      const userData = {name, email, text, id }
+  
+      const response = await fetch("http://localhost:3000/webhook", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({userData })
+      })
+      const data = await response.json();
+      console.log(data);
+    }
+
+    const onConfirm = async (event: React.FormEvent<HTMLFormElement>) => {
+      if (!stripe || !elements) {
+        // Stripe.js or Elements haven't loaded yet.
+        // Make sure to disable form submission until they have loaded.
+        return;
+      }
+
+      const { error: submitError } = await elements.submit();
+      if (submitError) {
+        setErrorMessage(submitError.message || "");
+        return;
+      }
+
+      // Create the PaymentIntent and obtain clientSecret
+      const res = await fetch('/create-intent', {
+        method: 'POST',
+      });
+      const { client_secret: clientSecret } = await res.json();
+  
+      // Confirm the PaymentIntent using the details collected by the Express Checkout Element
+      const {error} = await stripe.confirmPayment({
+        // `elements` instance used to create the Express Checkout Element
+        elements,
+        // `clientSecret` from the created PaymentIntent
+        clientSecret,
+        confirmParams: {
+          return_url: 'https://example.com/order/123/complete',
+        },
+      });
+  
+      if (error) {
+        // This point is only reached if there's an immediate error when
+        // confirming the payment. Show the error to your customer (for example, payment details incomplete)
+        setErrorMessage(error.message || "");
+      } else {
+        // The payment UI automatically closes with a success animation.
+        // Your customer is redirected to your `return_url`.
+      }
+    };
+  
+  
+    function generateRandomString() {
+      let result = '';
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      const charactersLength = characters.length;
+      for (let i = 0; i < 10; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      setId(result);
+    }
+  
+  
+    async function webHook (e: React.FormEvent<HTMLFormElement>, data: any) {
+      e.preventDefault();
+  
+      console.log(data)
+      const response = await fetch("http://localhost:3500/webhook", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+      })
+  
+      console.log(response)
+    }
+  
+    async function jobAlert(e: React.FormEvent<HTMLButtonElement>) {
+      e.preventDefault();
+      const userData = {
+        name: "John",
+        email: "testin@hola.com",
+      }
+  
+      const formElement = document.createElement('form');
+      const event = new Event('submit');
+      formElement.dispatchEvent(event);
+      await webHook(event as unknown as React.FormEvent<HTMLFormElement>, userData);
+    }
+  
+    return (
+      <>
+        <form className='form_Wrapper' onSubmit={updatingUser}>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" onChange={e => setName(e.target.value)} />
+          <label htmlFor="name">Email</label>
+          <input type="email" id="email" name="email" onChange={e => setEmail(e.target.value)}/>
+          <br />
+          <label htmlFor="email">Password</label>
+          <input type="password" id="email" name="password"  onChange={e => setPassword(e.target.value)}/>
+          <label htmlFor="name">Text </label>
+          <input type="text" id="text" name="text" onChange={e => setText(e.target.value)} />
+          <button type="submit">Submit</button>
+  
+        </form>
+        
+      <div>
+
+        <h1>Start a new job with a new technician</h1>
+        <button onClick={jobAlert}>Start!</button>
+
+      </div>
+      <div>
+        <h1>Testing Google/Apple Pay</h1>
+
+
+
+              <ExpressCheckoutElement onConfirm={onConfirm as unknown as (event: StripeExpressCheckoutElementConfirmEvent) => any} />
+       </div>
+          </>
+      
+       
+    )
+};
